59bf39f0becbbc1711b922832fe1bfe3e8e7456e
MUKILTEO\1721000 <NUGGETis1004@gmail.com>
2024-10-03 13:19:26 -0700

    startup of basic dialogue

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
+
+            
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            
+            string input = Console.ReadLine();
+            if (input == "")
+            {
+                Console
+            }
+            else if (input == "")
+            {
+
+            }
+            else if (input == "")
+            {
+
+            }
+            else if (input == "")
+            {
+                Console.WriteLine("");
+            }
         }
     }
 }
